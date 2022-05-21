66fc0efde5622dd81a0106f63b5dd34033f86dfe
Robert <robertpopescu1903@gmail.com>
2022-05-21 19:13:58 +0300

    Implemented account details page functionality.

---
+++ src/js/AccountDetailsHelper.js
@@ -0,0 +1,53 @@
+const userModule = require('./modules/User.js');
+const helperModule = require("./modules/Helper");
+
+async function getAccountDetails(username, tokens) {
+    const response = await userModule.getAccountDetails(username, tokens);
+    const alertPlaceholder = document.getElementById('errorAlertPlaceholder');
+
+    if(response.status == 200) {
+        const account = response;
+        const employee = account.employee;
+
+        const accountCreationDate = new Date(Date.parse(account.dateCreated));
+        const accountCreationString = `${accountCreationDate.getDate()}-${accountCreationDate.getMonth()}-${accountCreationDate.getFullYear()}`;
+
+        const empCreationDate = new Date(Date.parse(employee.bday));
+        const empCreationString = `${empCreationDate.getDate()}-${empCreationDate.getMonth()}-${empCreationDate.getFullYear()}`;
+
+        const roles = account.roles;
+
+        const accDetails = `
+            <p class="lead text-center" style="color: #0d6efd"><strong>Account</strong></p>
+            
+            <p class="lead">Username: ${account.username}</p>
+            <p class="lead">email: ${account.email}</p>
+            <p class="lead">Creation date: ${accountCreationString}</p>
+            <p class="lead">Role: ${roles[0].name}</p><br>
+            
+            <p class="lead text-center" style="color: #0d6efd"><strong>Employee</strong></p>
+            
+            <p class="lead">First name: ${employee.firstName}</p>
+            <p class="lead">Last name: ${employee.lastName}</p>
+            <p class="lead">Phone: ${employee.phone}</p>
+            <p class="lead">CNP: ${employee.cnp}</p>
+            <p class="lead">Birthday: ${empCreationString}</p>
+        `;
+
+        const accCard = `
+            <div class="card">
+                <div class="card-header text-white"></div>
+                <div class="card-body">
+                    ${accDetails}
+                </div>
+            </div>
+        `;
+
+        const placeholder = document.getElementById('account');
+        placeholder.innerHTML = accCard;
+    } else {
+        helperModule.showAlert('Account details could not be fetched because of a server problem.', 'danger', alertPlaceholder);
+    }
+}
+
+global.window.getAccountDetails = getAccountDetails;
--- src/js/modules/User.js
+++ src/js/modules/User.js
@@ -192,7 +192,54 @@ async function createEmployee(account, employee, tokens) {
     }
 }
 
+async function getAccountDetails(username, tokens) {
+    tokens = await helperModule.redirectToLogin(tokens);
+
+    if(tokens != false) {
+        const url = `http://localhost:8080/user/main/organisation/account/${username}`;
+        const config = {
+            headers: {
+                'Authorization': `Token: ${tokens.accessToken}`
+            }
+        };
+        let response;
+
+        try {
+            await axios
+                .get(url, config)
+                .then(function (resp) {
+                    console.log(resp);
+                    response = {
+                        status: resp.status,
+                        username: resp.data.username,
+                        email: resp.data.email,
+                        dateCreated: resp.data.dateCreated,
+                        employee: resp.data.employee,
+                        roles: resp.data.roles
+                    }
+                })
+                .catch(function (err) {
+                    console.log(err);
+                    response = {
+                        status: err.response.status,
+                        message: err.message,
+                        serverMessage: err.response.data.error_message
+                    }
+                });
+        } catch (err) {
+            response = {
+                status: 900,
+                message: err.message,
+                serverMessage: 'Internal error.'
+            }
+        }
+
+        return response;
+    }
+}
+
 module.exports.getOrganisation = getOrganisation;
 module.exports.getOrganisationEmployees = getOrganisationEmployees;
 module.exports.createDepartment = createDepartment;
 module.exports.createEmployee = createEmployee;
+module.exports.getAccountDetails = getAccountDetails;
